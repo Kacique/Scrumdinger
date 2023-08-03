08584bd7891f75a80ac02c68ee7b755e1fcd927a
Kacique <wbrand90@gmail.com>
2023-08-03 11:17:28 -0700

    Created navigation hierarchy

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -10,14 +10,18 @@ import Foundation
 struct DailyScrum: Identifiable {
     var id: UUID
     var title: String
-    var attendees: [String]
+    var attendees: [Attendee]
     var lengthInMinutes: Int
     var theme: Theme
     
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
         self.id = id
         self.title = title
-        self.attendees = attendees
+        /*
+         map(_:) creates a new collection by iterating over and applying a transformation to
+         each element in an existing collection.
+         */
+        self.attendees = attendees.map{Attendee(name: $0)}
         self.lengthInMinutes = lengthInMinutes
         self.theme = theme
     }
@@ -56,4 +60,16 @@ extension DailyScrum{
                    lengthInMinutes: 5,
                    theme: .poppy)
     ]
+}
+
+extension DailyScrum{
+    struct Attendee: Identifiable {
+        let id: UUID
+        var name: String
+        
+        init(id: UUID = UUID(), name: String){
+            self.id = id
+            self.name = name
+        }
+    }
 }
